da6e72e0c93ccaf07345dbec80ebbc120b509375
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-03-01 09:51:10 +0100

    Adding base and gitignore

--- src/computer_vision/CVSubscriber.cpp
+++ src/computer_vision/CVSubscriber.cpp
@@ -15,6 +15,51 @@
 */
 
 #include "computer_vision/CVSubscriber.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+// -------- Parameters ----------------------------------------------
+
+namespace CVParams {
+
+  inline bool running = false;
+
+  inline std::string WINDOW_NAME = "Practica_5";
+  inline std::string WINDOW_HIST_NAME = "Histograms";
+
+  inline std::string MODE = "Option [0-4]";
+  inline std::string SHRINK_MIN = "Shrink min value [0-127]";
+  inline std::string SHRINK_MAX = "Shrink max value [128-255]";
+  inline std::string HOUGH = "Hough accumulator [0-255]";
+  inline std::string AREA = "Area [0-500]";
+
+  float PI = 3.14159265;
+}
+
+// -------- Self-Made Functions ----------------------------------------------
+namespace CVFunctions {
+
+// -------- Window Management Functions ----------------------------------------------
+void initWindow()
+// Create window at the beggining
+{
+  if (CVParams::running) return;
+  CVParams::running = true;
+
+  // Show images in a different windows
+  cv::namedWindow(CVParams::WINDOW_NAME);
+  // create Trackbar and add to a window
+  cv::createTrackbar(CVParams::MODE, CVParams::WINDOW_NAME, nullptr, 4, 0); 
+  cv::createTrackbar(CVParams::SHRINK_MIN, CVParams::WINDOW_NAME, nullptr, 127, 0);
+  cv::createTrackbar(CVParams::SHRINK_MAX, CVParams::WINDOW_NAME, nullptr, 127, 0);
+  cv::createTrackbar(CVParams::HOUGH, CVParams::WINDOW_NAME, nullptr, 255, 0);
+  cv::createTrackbar(CVParams::AREA, CVParams::WINDOW_NAME, nullptr, 500, 0);
+}
+
+}
 
 namespace computer_vision
 {
@@ -28,6 +73,8 @@ CVGroup CVSubscriber::processing(
   const pcl::PointCloud<pcl::PointXYZRGB> in_pointcloud)
 const
 {
+  int mode_param;
+
   // Create output images
   cv::Mat out_image_rgb, out_image_depth;
   // Create output pointcloud
@@ -38,9 +85,31 @@ const
   out_image_depth = in_image_depth;
   out_pointcloud = in_pointcloud;
 
-  // Show images in a different windows
-  cv::imshow("out_image_rgb", out_image_rgb);
-  cv::imshow("out_image_depth", out_image_depth);
+  // First time execution
+  CVFunctions::initWindow();
+
+  // Obtaining Parameter
+  mode_param = cv::getTrackbarPos(CVParams::MODE, CVParams::WINDOW_NAME);
+
+  switch (mode_param)
+  {
+  case 1:
+    cv::imshow(CVParams::WINDOW_NAME, out_image_rgb);
+    break;
+  case 2:
+    cv::imshow(CVParams::WINDOW_NAME, out_image_rgb);
+    break;
+  case 3:
+    cv::imshow(CVParams::WINDOW_NAME, out_image_rgb);
+    break;
+  case 4:
+    cv::imshow(CVParams::WINDOW_NAME, out_image_rgb);
+    break;
+  default:
+    cv::imshow(CVParams::WINDOW_NAME, out_image_rgb);
+    break;
+  }
+
   cv::waitKey(3);
 
   return CVGroup(out_image_rgb, out_image_depth, out_pointcloud);
